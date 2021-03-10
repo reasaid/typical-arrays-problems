fbfc2c7e107b433f51370e14147840e11b7ed326
Maria Lobanova <roldik95@gmail.com>
2021-03-10 18:09:17 +0300

    feat: add min, max and avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array)){
+    if(array.length !==0){
+   array.sort((a,b)=>a-b);
+   return array[0];
+  } }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array)){
+    if(array.length !==0){
+    array.sort((a,b)=>a-b);
+    return array[(array.length-1)];
+  } }
   return 0;
 }
 
+
 exports.avg = function avg (array) {
+  if (Array.isArray(array)){
+    if(array.length !==0){
+   return (array.reduce((acc,item)=>acc + item)) / (array.length);
+  } }
   return 0;
 }
+
